132a2d2aa33ff141abc1aa22bea290ac9557eff0
allweareisdustinthewind <tololo_ilya@web.de>
2026-04-27 08:11:03 +0200

    Realize calculator functionality in separat module

--- Program.cs
+++ Program.cs
@@ -1,47 +1,18 @@
 ﻿using System.Text.RegularExpressions;
-
-class Calculator
-{
-   public static double DoOperation (double num1, double num2, string op)
-   {
-      double result = double.NaN; // Default value is "not-a-number" if an operation, such as division, could result in an error.
-
-      // Use a switch statement to do the math.
-      switch (op)
-      {
-         case "a":
-            result = num1 + num2;
-            break;
-         case "s":
-            result = num1 - num2;
-            break;
-         case "m":
-            result = num1 * num2;
-            break;
-         case "d":
-            // Ask the user to enter a non-zero divisor.
-            if (num2 != 0)
-            {
-               result = num1 / num2;
-            }
-            break;
-         // Return text for an incorrect option entry.
-         default:
-            break;
-      }
-      return result;
-   }
-}
+using CalculatorLibrary;
+using System.Diagnostics;
 
 class Program
 {
    static void Main (string [] args)
    {
       bool endApp = false;
+
       // Display title as the C# console calculator app.
       Console.WriteLine ("Console Calculator in C#\r");
       Console.WriteLine ("------------------------\n");
 
+      Calculator calculator = new ();
       while (!endApp)
       {
          // Declare variables and set to empty.
@@ -84,33 +55,34 @@ class Program
 
          // Validate input is not null, and matches the pattern
          if (op == null || !Regex.IsMatch (op, "[a|s|m|d]"))
-         {
             Console.WriteLine ("Error: Unrecognized input.");
-         }
          else
          {
             try
             {
-               result = Calculator.DoOperation (cleanNum1, cleanNum2, op);
+               result = calculator.DoOperation (cleanNum1, cleanNum2, op);
                if (double.IsNaN (result))
-               {
                   Console.WriteLine ("This operation will result in a mathematical error.\n");
-               }
-               else Console.WriteLine ("Your result: {0:0.##}\n", result);
+               else 
+                  Console.WriteLine ("Your result: {0:0.##}\n", result);
             }
             catch (Exception e)
             {
                Console.WriteLine ("Oh no! An exception occurred trying to do the math.\n - Details: " + e.Message);
             }
          }
+
          Console.WriteLine ("------------------------\n");
 
          // Wait for the user to respond before closing.
          Console.Write ("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
-         if (Console.ReadLine () == "n") endApp = true;
+         if (Console.ReadLine () == "n") 
+            endApp = true;
 
          Console.WriteLine ("\n"); // Friendly linespacing.
       }
+
+      calculator.Finish ();
       return;
    }
 }
